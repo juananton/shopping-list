3ca803279dff886aaa2cce844bda3c58ea081ccf
juananton <juananton.correo@gmail.com>
2022-12-16 15:26:45 +0100

    Modal component fixes

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -3,12 +3,19 @@ import { FiX } from 'react-icons/fi';
 import Button from './Button';
 
 const Modal = ({ title, children, closeModal }) => {
+	if (!children) return null;
+
 	return createPortal(
 		<div className='modal-overlay'>
 			<div className='modal-window'>
 				<div className='header'>
 					<h1>{title}</h1>
-					<Button className='close' variant='icon' onClick={closeModal}>
+					<Button
+						className='close'
+						variant='icon'
+						use='nobg'
+						onClick={closeModal}
+					>
 						<FiX className='icon' />
 					</Button>
 				</div>
