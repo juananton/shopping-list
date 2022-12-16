ccdee777952f5eefdffc151821af77aeca9aef4d
juananton <juananton.correo@gmail.com>
2022-12-16 12:44:39 +0100

    Add configuration to button component

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,7 +1,29 @@
-const Button = props => {
+const BTN_VARIANTS = {
+	text: 'btn-text',
+	icon: 'btn-icon'
+};
+const BTN_USES = {
+	primary: 'btn-primary',
+	secondary: 'btn-secondary',
+	disabled: 'disabled'
+};
+
+const Button = ({
+	variant = 'text',
+	use = 'secondary',
+	className,
+	children,
+	...props
+}) => {
+	const btnVariant = BTN_VARIANTS[variant];
+	const btnUse = BTN_USES[use];
+
 	return (
-		<button {...props} className='btn icon-btn'>
-			{props.children}
+		<button
+			{...props}
+			className={`btn ${btnVariant} ${btnUse} ${className} || ''`}
+		>
+			{children}
 		</button>
 	);
 };
