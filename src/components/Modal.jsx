ea85ebe60d94b4ae2ed054b71def271c5aee4754
juananton <juananton.correo@gmail.com>
2022-12-20 21:23:30 +0100

    Add footer to modal component

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -2,14 +2,14 @@ import { createPortal } from 'react-dom';
 import { FiX } from 'react-icons/fi';
 import Button from './Button';
 
-const Modal = ({ title, children, closeModal }) => {
+const Modal = ({ formTitle, children, closeModal, formId }) => {
 	if (!children) return null;
 
 	return createPortal(
 		<div className='modal-overlay'>
 			<div className='modal-window'>
-				<div className='header'>
-					<h1>{title}</h1>
+				<div className='modal-window-header'>
+					<h1>{`${formTitle} ítem`}</h1>
 					<Button
 						className='close'
 						variant='icon'
@@ -20,6 +20,14 @@ const Modal = ({ title, children, closeModal }) => {
 					</Button>
 				</div>
 				{children}
+				<div className='modal-window-footer'>
+					<Button type='button' onClick={closeModal}>
+						Cancelar
+					</Button>
+					<Button type='submit' use='primary' form={formId}>
+						{formTitle}
+					</Button>
+				</div>
 			</div>
 		</div>,
 		document.getElementById('modal')
